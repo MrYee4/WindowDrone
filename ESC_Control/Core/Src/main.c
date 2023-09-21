463400a9a606d100085acd113ab1d255abe45fdd
Spencer Nettles <net17002@byui.edu>
2023-09-21 17:09:51 -0600

    Updated STM IDE

--- ESC_Control/Core/Src/main.c
+++ ESC_Control/Core/Src/main.c
@@ -414,9 +414,9 @@ static void MX_TIM16_Init(void)
 
   /* USER CODE END TIM16_Init 1 */
   htim16.Instance = TIM16;
-  htim16.Init.Prescaler = 9999;
+  htim16.Init.Prescaler = 1;
   htim16.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim16.Init.Period = 1599;
+  htim16.Init.Period = 3999;
   htim16.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim16.Init.RepetitionCounter = 0;
   htim16.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
